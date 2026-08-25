4fb0d4bca7bc4d8b2cde7129b2e0d0daca3ebd41
kadudivyang <divyangkadu282@gmail.com>
2026-08-25 11:05:05 +0530

    Update sesion 6.py

--- Assignments/sesion 6.py
+++ Assignments/sesion 6.py
@@ -1,22 +1,47 @@
-#fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"]
-
-#for fruit in fruits:
-#    if fruit == "Banana":
-#        break
-#    print(fruit)
-
-fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"]
-
-for fruit in fruits:
-    if fruit == "Banana":
-        continue
-    print(fruit)
+# num = 1
+#
+# while num <= 10:
+#     print(num)
+#     num += 1
 
 
-       fruits = ["Apple", "Mango", "Banana"]
+# minutes_left = 5
+#
+# while minutes_left >= 0:
+#     print("Offer ends in", minutes_left, "minutes")
+#     minutes_left -= 1
 
 
-        for fruit in fruits:
-           if fruit == "Banana":
-                pass
-            print(fruit)
+# row = 1
+#
+# while row <= 5:
+#     print("*" * row)
+#     row += 1
+
+# count = 0
+#
+# while True:
+#     print("Loading...")
+#     count += 1
+#
+#     if count == 3:
+#         break
+
+# rows = 4
+# i = 1
+#
+# while i <= rows:
+#     # Print spaces
+#     spaces = 1
+#     while spaces <= rows - i:
+#         print(" ", end="")
+#         spaces += 1
+#
+#     # Print stars
+#     stars = 1
+#     while stars <= (2 * i - 1):
+#         print("*", end="")
+#         stars += 1
+#
+#     print()
+#     i += 1
