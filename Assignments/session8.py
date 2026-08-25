ce7277c88370a4a8d490143d8c80485af03acbc8
kadudivyang <divyangkadu282@gmail.com>
2026-08-25 10:59:18 +0530

    Update session8.py

--- Assignments/session8.py
+++ Assignments/session8.py
@@ -1,6 +1,50 @@
-fruits = ['Apple', 'Banana', 'Mango', 'Orange']
+# insta_caption.py
+#
+# caption = input("Enter your Instagram caption: ")
+#
+# print("First 10 characters:", caption[:10])
 
-for fruit in fruits:
-    if fruit == 'Banana':
-        continue
-    print(fruit)
+# def extract_artist(song_title):
+#     dash_position = song_title.index("-")
+#     artist = song_title[dash_position + 1:]
+#     return artist.strip()
+#
+#
+# # Example
+# song_title = "Shape of You - Ed Sheeran"
+# print(extract_artist(song_title))
+
+
+# def reverse_message(message):
+#     reversed_text = ""
+#
+#     for char in message:
+#         reversed_text = char + reversed_text
+#
+#     return reversed_text
+#
+#
+# # Example
+# message = input("Enter a message: ")
+# print("Reversed message:", reverse_message(message))
+
+
+# product_description = "Wireless Bluetooth Headphones with Noise Cancellation"
+#
+# words = product_description.split()
+#
+# first_word = words[0]
+# last_word = words[-1]
+# total_words = len(words)
+#
+# print("First word:", first_word)
+# print("Last word:", last_word)
+# print("Total number of words:", total_words)
+
+# def mask_phone_number(phone):
+#     return "*" * 6 + phone[-4:]
+#
+#
+# # Example
+# phone = input("Enter your 10-digit phone number: ")
+# print("Masked phone number:", mask_phone_number(phone))
