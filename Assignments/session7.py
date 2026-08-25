4af548aacee61f598d5709a89114d45f925e14a3
kadudivyang <divyangkadu282@gmail.com>
2026-08-25 10:58:12 +0530

    Update session7.py

--- Assignments/session7.py
+++ Assignments/session7.py
@@ -1,2 +1,33 @@
-caption = input("enter your instagram caption: ")
-print ("frist 10 character of instgram caption :",caption[:10])
+# fruits = ['Apple', 'Banana', 'Mango', 'Orange']
+#
+# for fruit in fruits:
+#     if fruit == 'Banana':
+#         continue
+#     print(fruit)
+
+foods = ['Pizza', 'Burger', 'Pasta', 'Sandwich', 'Burger King']
+
+# for food in foods:
+#     if food == 'Burger King':
+#         print("Found Burger King, stopping search.")
+#         break
+#     print(food)
+
+# playlists = ['Chill Vibes', 'Workout', 'Focus', 'Party']
+#
+# for playlist in playlists:
+#     if playlist == 'Focus':
+#         pass
+#     else:
+#         print(playlist)
+
+# messages = ['Hi', 'Spam', 'Hello', 'Spam', 'How are you?']
+#
+# for message in messages:
+#     if message == 'Spam':
+#         continue
+#
+#     if message == 'How are you?':
+#         break
+#
+#     print(message)
