cdbffd44cc35dd6655ac89329e1c1f0c72520553
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:53:25 +0530

    Update session8.py

--- Assignments/session8.py
+++ Assignments/session8.py
@@ -1,48 +1,48 @@
-# insta_caption.py
-#
-# caption = input("Enter your Instagram caption: ")
-#
-# print("First 10 characters:", caption[:10])
-
-# def extract_artist(song_title):
-#     dash_position = song_title.index("-")
-#     artist = song_title[dash_position + 1:]
-#     return artist.strip()
-#
-#
-# # Example
-# song_title = "Shape of You - Ed Sheeran"
-# print(extract_artist(song_title))
 
 
-# def reverse_message(message):
-#     reversed_text = ""
-#
-#     for char in message:
-#         reversed_text = char + reversed_text
-#
-#     return reversed_text
-#
-#
-# # Example
-# message = input("Enter a message: ")
-# print("Reversed message:", reverse_message(message))
+caption = input("Enter your Instagram caption: ")
+
+print("First 10 characters:", caption[:10])
+
+def extract_artist(song_title):
+    dash_position = song_title.index("-")
+    artist = song_title[dash_position + 1:]
+    return artist.strip()
 
 
-# product_description = "Wireless Bluetooth Headphones with Noise Cancellation"
-#
-# words = product_description.split()
-#
-# first_word = words[0]
-# last_word = words[-1]
-# total_words = len(words)
-#
-# print("First word:", first_word)
-# print("Last word:", last_word)
-# print("Total number of words:", total_words)
+# Example
+song_title = "Shape of You - Ed Sheeran"
+print(extract_artist(song_title))
 
-# def mask_phone_number(phone):
-#     return "*" * 6 + phone[-4:]
-# # Example
-# phone = input("Enter your 10-digit phone number: ")
-# print("Masked phone number:", mask_phone_number(phone))
+
+def reverse_message(message):
+    reversed_text = ""
+
+    for char in message:
+        reversed_text = char + reversed_text
+
+    return reversed_text
+
+
+# Example
+message = input("Enter a message: ")
+print("Reversed message:", reverse_message(message))
+
+
+product_description = "Wireless Bluetooth Headphones with Noise Cancellation"
+
+words = product_description.split()
+
+first_word = words[0]
+last_word = words[-1]
+total_words = len(words)
+
+print("First word:", first_word)
+print("Last word:", last_word)
+print("Total number of words:", total_words)
+
+def mask_phone_number(phone):
+    return "*" * 6 + phone[-4:]
+# Example
+phone = input("Enter your 10-digit phone number: ")
+print("Masked phone number:", mask_phone_number(phone))
