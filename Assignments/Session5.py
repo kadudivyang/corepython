c894400020f46d8a91e637c2e6de308704c5deef
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:50:08 +0530

    Update Session5.py

--- Assignments/Session5.py
+++ Assignments/Session5.py
@@ -1,28 +1,28 @@
-# food_apps = ["Zomato", "Swiggy", "Domino's", "McDonald's", "Pizza Hut"]
-#
-# for app in food_apps:
-#     print(app)
+food_apps = ["Zomato", "Swiggy", "Domino's", "McDonald's", "Pizza Hut"]
+
+for app in food_apps:
+    print(app)
 
 
-# user_bio = "Music lover | Foodie | Traveller"
-#
-# count = 0
-#
-# for char in user_bio:
-#     if char != " ":
-#         count += 1
-#
-# print("Number of characters excluding spaces:", count)
+user_bio = "Music lover | Foodie | Traveller"
 
-# fav_movies = ["3 Idiots", "Dangal", "KGF"]
-#
-# for movie in fav_movies:
-#     print(movie.upper())
+count = 0
 
-#
-# word = input("Enter a word: ")
-#
-# for char in word:
-#     if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
-#         print(char)
+for char in user_bio:
+    if char != " ":
+        count += 1
+
+print("Number of characters excluding spaces:", count)
+
+fav_movies = ["3 Idiots", "Dangal", "KGF"]
+
+for movie in fav_movies:
+    print(movie.upper())
+
+
+word = input("Enter a word: ")
+
+for char in word:
+    if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
+        print(char)
 
