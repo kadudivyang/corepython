6c76a7ebf4796e16a8552bb5e5b9a80150e35d12
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:41:59 +0530

    Update session2.py

--- Assignments/session2.py
+++ Assignments/session2.py
@@ -1,48 +1,40 @@
-# product_name = "Flipkart Smartphone"
-# price = 19999
-# is_available = True
-# rating = 4.5
-#
-# print("Product Name:", product_name, type(product_name))
-# print("Price:", price, type(price))
-# print("Is Available:", is_available, type(is_available))
-# print("Rating:", rating, type(rating))
+product_name = "Flipkart Smartphone"
+price = 19999
+is_available = True
+rating = 4.5
+
+print("Product Name:", product_name, type(product_name))
+print("Price:", price, type(price))
+print("Is Available:", is_available, type(is_available))
+print("Rating:", rating, type(rating))
 
 
-# restaurant_name = "Spice Villa"
-# cuisine_type = "Indian"
-# average_cost = 500
-# is_open_now = True
-#
-# print("Restaurant Name:", restaurant_name)
-# print("Cuisine Type:", cuisine_type)
-# print("Average Cost:", average_cost)
-# print("Is Open Now:", is_open_now)
+restaurant_name = "Spice Villa"
+cuisine_type = "Indian"
+average_cost = 500
+is_open_now = True
+
+print("Restaurant Name:", restaurant_name)
+print("Cuisine Type:", cuisine_type)
+print("Average Cost:", average_cost)
+print("Is Open Now:", is_open_now)
 
 
-# playlist_title = "My Favorite Songs"
-# number_of_songs = 25
-# is_public = True
-#
-# print("Playlist Title:", playlist_title)
-# print("Memory Address:", id(playlist_title))
-#
-# print("Number of Songs:", number_of_songs)
-# print("Memory Address:", id(number_of_songs))
-#
-# print("Is Public:", is_public)
-# print("Memory Address:", id(is_public))
+playlist_title = "My Favorite Songs"
+number_of_songs = 25
+is_public = True
+
+print("Playlist Title:", playlist_title)
+print("Memory Address:", id(playlist_title))
+
+print("Number of Songs:", number_of_songs)
+print("Memory Address:", id(number_of_songs))
+
+print("Is Public:", is_public)
+print("Memory Address:", id(is_public))
 
 title = "Python Tutorial"
 views = 100000
 likes = 5000
 is_premium = False
 
-# Primitive data types:
-# title is a string (str), which stores text.
-# views is an integer (int), which stores a whole number.
-# likes is an integer (int), which stores a whole number.
-# is_premium is a boolean (bool), which stores True or False.
-
-# In Python, these are basic/primitive data types.
-# There are no non-primitive data types in the given variables.
