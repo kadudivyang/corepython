d03962109cce0c7f9f00e6409ad5f19b45508197
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:48:11 +0530

    Update Session3.py

--- Assignments/Session3.py
+++ Assignments/Session3.py
@@ -1,27 +1,27 @@
-# name = input("Enter your name: ")
-# favorite_food = input("Enter your favorite food: ")
-#
-# print(f"Hello {name}, your favorite food is {favorite_food}!")
+name = input("Enter your name: ")
+favorite_food = input("Enter your favorite food: ")
+
+print(f"Hello {name}, your favorite food is {favorite_food}!")
 
 
-# num1 = int(input("Enter first number: "))
-# num2 = int(input("Enter second number: "))
-#
-# print("Sum:", num1 + num2)
-# print("Difference:", num1 - num2)
-# print("Product:", num1 * num2)
-# print("Quotient:", num1 / num2)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
-# price = float(input("Enter the price of the food item: "))
-# quantity = int(input("Enter the quantity: "))
-#
-# total_bill = price * quantity
-#
-# print(f"Your total bill is ₹{total_bill:.2f}")
+print("Sum:", num1 + num2)
+print("Difference:", num1 - num2)
+print("Product:", num1 * num2)
+print("Quotient:", num1 / num2)
+
+price = float(input("Enter the price of the food item: "))
+quantity = int(input("Enter the quantity: "))
+
+total_bill = price * quantity
+
+print(f"Your total bill is ₹{total_bill:.2f}")
 
 
-# followers = int(input("Enter your Instagram follower count: "))
-# print("\n\tYou have {:,} followers".format(followers))
+followers = int(input("Enter your Instagram follower count: "))
+print("\n\tYou have {:,} followers".format(followers))
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
