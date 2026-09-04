85a75c284d889d83794d4538a3b00c3941c874fd
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:55:38 +0530

    Update sesion10.py

--- Assignments/sesion10.py
+++ Assignments/sesion10.py
@@ -1,25 +1,25 @@
-# fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
-# print(fav_apps)
+fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
+print(fav_apps)
 
-# fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
-# print("2nd app:", fav_apps[1])
-# print("4th app:", fav_apps[3}
-
-
-# fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
-# # Trying to change the first element
-# fav_apps[0] = "YouTube"
-# # Error: TypeError: 'tuple' object does not support item assignment
-# # This happens because tuples are immutable, so their elements cannot be changed.
+fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
+print("2nd app:", fav_apps[1])
+print("4th app:", fav_apps[2])
 
 
-# fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
-# # Print the middle three apps
-# print(fav_apps[1:4])
+fav_apps = ["Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart"]
+# Trying to change the first element
+fav_apps[0] = "YouTube"
+# Error: TypeError: 'tuple' object does not support item assignment
+# This happens because tuples are immutable, so their elements cannot be changed.
 
 
-# fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
-# new_apps = ("Telegram", "Netflix")
-# all_apps = fav_apps + new_apps
-# print(all_apps)
+fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
+# Print the middle three apps
+print(fav_apps[1:4])#
+
+
+fav_apps = ("Instagram", "Zomato", "Spotify", "WhatsApp", "Flipkart")
+new_apps = ("Telegram", "Netflix")
+all_apps = fav_apps + new_apps
+print(all_apps)
 
