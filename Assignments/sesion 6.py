2b18b12fd315922b83a86e6a2ab78dc8a3a971ac
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:50:57 +0530

    Update sesion 6.py

--- Assignments/sesion 6.py
+++ Assignments/sesion 6.py
@@ -1,47 +1,47 @@
-# num = 1
-#
-# while num <= 10:
-#     print(num)
-#     num += 1
+num = 1
+
+while num <= 10:
+    print(num)
+    num += 1
 
 
-# minutes_left = 5
-#
-# while minutes_left >= 0:
-#     print("Offer ends in", minutes_left, "minutes")
-#     minutes_left -= 1
+minutes_left = 5
+
+while minutes_left >= 0:
+    print("Offer ends in", minutes_left, "minutes")
+    minutes_left -= 1
 
 
-# row = 1
-#
-# while row <= 5:
-#     print("*" * row)
-#     row += 1
+row = 1
 
-# count = 0
-#
-# while True:
-#     print("Loading...")
-#     count += 1
-#
-#     if count == 3:
-#         break
+while row <= 5:
+    print("*" * row)
+    row += 1
 
-# rows = 4
-# i = 1
-#
-# while i <= rows:
-#     # Print spaces
-#     spaces = 1
-#     while spaces <= rows - i:
-#         print(" ", end="")
-#         spaces += 1
-#
-#     # Print stars
-#     stars = 1
-#     while stars <= (2 * i - 1):
-#         print("*", end="")
-#         stars += 1
-#
-#     print()
-#     i += 1
+count = 0
+
+while True:
+    print("Loading...")
+    count += 1
+
+    if count == 3:
+        break
+
+rows = 4
+i = 1
+
+while i <= rows:
+    # Print spaces
+    spaces = 1
+    while spaces <= rows - i:
+        print(" ", end="")
+        spaces += 1
+
+    # Print stars
+    stars = 1
+    while stars <= (2 * i - 1):
+        print("*", end="")
+        stars += 1
+
+    print()
+    i += 1
