22897ae059cac5220d74af82a2d34258528682f6
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:56:23 +0530

    Update sesion12.py

--- Assignments/sesion12.py
+++ Assignments/sesion12.py
@@ -1,35 +1,35 @@
-# def get_discounted_price(price, discount_percent):
-#     discount = price * discount_percent / 100
-#     final_price = price - discount
-#     return final_price
-# # Test the function
-# price = 500
-# discount_percent = 10
-# print("Final price:", get_discounted_price(price, discount_percent))
-
-
-# def format_follower_count(number):
-#     if number >= 1000000:
-#         return f"{number / 1000000:.1f}M"
-#     elif number >= 1000:
-#         return f"{number / 1000:.1f}K"
-#     else:
-#         return str(number)
-# # Test the function
-# print(format_follower_count(1500))
-# print(format_follower_count(1200000))
-# print(format_follower_count(850))
+def get_discounted_price(price, discount_percent):
+    discount = price * discount_percent / 100
+    final_price = price - discount
+    return final_price
+# Test the function
+price = 500
+discount_percent = 10
+print("Final price:", get_discounted_price(price, discount_percent))
 
 #
-# song_durations = [3, 4, 5, 2.5]
-# # Convert minutes to seconds using lambda and map()
-# durations_in_seconds = list(map(lambda minutes: minutes * 60, song_durations))
-# print(durations_in_seconds)
+def format_follower_count(number):
+    if number >= 1000000:
+        return f"{number / 1000000:.1f}M"
+    elif number >= 1000:
+        return f"{number / 1000:.1f}K"
+    else:
+        return str(number)
+# Test the function
+print(format_follower_count(1500))
+print(format_follower_count(1200000))
+print(format_follower_count(850))
 
-# products = ["Mobile", "Mouse", "Laptop", "Monitor", "Keyboard"]
-# # Filter products starting with 'M'
-# m_products = list(filter(lambda product: product.startswith("M"), products))
-# print(m_products)
+
+song_durations = [3, 4, 5, 2.5]
+# Convert minutes to seconds using lambda and map()
+durations_in_seconds = list(map(lambda minutes: minutes * 60, song_durations))
+print(durations_in_seconds)
+
+products = ["Mobile", "Mouse", "Laptop", "Monitor", "Keyboard"]
+# Filter products starting with 'M'
+m_products = list(filter(lambda product: product.startswith("M"), products))
+print(m_products)
 
 
 from functools import reduce
