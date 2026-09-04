d553bea683f6089cb1200ba84cf51300926454a4
kadudivyang <divyangkadu282@gmail.com>
2026-09-04 10:48:50 +0530

    Update session4.py

--- Assignments/session4.py
+++ Assignments/session4.py
@@ -1,33 +1,33 @@
-# age = int(input("Enter your age: "))
-#
-# if age >= 13:
-#     print("You are eligible to create an Instagram account.")
-# else:
-#     print("You are not eligible to create an Instagram account.")
+age = int(input("Enter your age: "))
+
+if age >= 13:
+    print("You are eligible to create an Instagram account.")
+else:
+    print("You are not eligible to create an Instagram account.")
 
 marks = int(input("Enter your marks (0-100): "))
 
-# if marks >= 90:
-#     print("Grade: A")
-# elif marks >= 75:
-#     print("Grade: B")
-# elif marks >= 60:
-#     print("Grade: C")
-# elif marks >= 40:
-#     print("Grade: D")
-# else:
-#     print("Grade: F")
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+elif marks >= 40:
+    print("Grade: D")
+else:
+    print("Grade: F")
 
-# age = int(input("Enter your age: "))
-# time = int(input("Enter the current time (0-23): "))
-#
-# if age >= 18:
-#     if time >= 22 or time <= 2:
-#         print("Order allowed")
-#     else:
-#         print("Order not allowed")
-# else:
-#     print("Order not allowed")
+age = int(input("Enter your age: "))
+time = int(input("Enter the current time (0-23): "))
+
+if age >= 18:
+    if time >= 22 or time <= 2:
+        print("Order allowed")
+    else:
+        print("Order not allowed")
+else:
+    print("Order not allowed")
 
 
 score = int(input("Enter your favorite cricket team's score: "))
